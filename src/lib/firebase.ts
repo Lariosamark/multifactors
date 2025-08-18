c18e70c62fdb51d673865e16f2f0b418bf7bae73
mark_lariosa <markanthonylariosa3@gmail.com>
2025-08-18 14:51:21 +0800

    pls

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-// src/lib/firebase.ts
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -17,5 +16,3 @@ export const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfi
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
-
-
